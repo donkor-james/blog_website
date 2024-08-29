6b7d5adb84d14a5768d2c9486728d68663049f21
Donkor James <jamesdonkor987@gmail.com>
2024-08-29 23:45:23 +0000

    updated

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.forms import BaseModelForm
 from .models import Post
+from django.contrib.auth.models import User
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
@@ -17,7 +20,19 @@ class PostListView(ListView):
     model = Post
     template_name = "blog/home.html"
     context_object_name = "posts"
-    ordering = ["date_posted"]
+    ordering = ["-date_posted"]
+    paginate_by = 4
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = "blog/user_posts.html"
+    context_object_name = "posts"
+    paginate_by = 4
+
+    def get_query_set(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by("-date_posted")
 
 
 class PostDetailView(DetailView):
