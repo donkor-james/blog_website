449958ec2fd03a664da81c35f0ad8fabf2572d45
Donkor James <jamesdonkor987@gmail.com>
2024-09-18 14:35:10 +0000

    modified settings.py, created context_processors.py and deleted some files

--- django_project/settings.py
+++ django_project/settings.py
@@ -77,7 +77,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'blog.te.show_hero'
+                'blog.context_processors.show_hero'
             ],
         },
     },
